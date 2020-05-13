343041edaa54732964059f1de16485ba7d30d333
Zeshan Shaikh <zee.shaikh0401@gmail.com>
2020-05-13 06:20:15 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import SearchBar from "./components/SearchBar";
+import youtubeapi from "./apis/youtubeapi";
+import VideoList from "./components/VideoList";
+import VideoDetails from "./components/VideoDetails";
+import TitleBar from "./components/TitleBar";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./components/VideoItem.css";
+
+class App extends React.Component {
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit("cars");
+  }
+
+  onTermSubmit = async (term) => {
+    const response = await youtubeapi.get("/search", {
+      params: {
+        part: "snippet",
+        maxResults: 10,
+        key: "AIzaSyCosmAnIVK7Ubvl-GcJQS9ECHW5h_UDbtc",
+        q: term,
+      },
+    });
+
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0],
+    });
+  };
+
+  onVideoSelect = (video) => {
+    this.setState({ selectedVideo: video });
+  };
+
+  render() {
+    return (
+      <div className="box">
+        <div className="ui container">
+          <div
+            className="ui center aligned grid"
+            style={{ padding : "25px", color: "#04278b" }}
+          >
+            <TitleBar />
+          </div>
+          <SearchBar onFormSubmit={this.onTermSubmit} />
+          <div className="ui grid">
+            <div className="ui row">
+              <div className="eleven wide column">
+                <VideoDetails video={this.state.selectedVideo} />
+              </div>
+              <div className="five wide column">
+                <VideoList
+                  onVideoSelect={this.onVideoSelect}
+                  videos={this.state.videos}
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TitleBar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const TitleBar = () => {
+    return(
+        <h1>Youtube Videos</h1>
+    );
+} 
+
+export default TitleBar;
