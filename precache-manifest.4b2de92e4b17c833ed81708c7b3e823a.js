7dd4f56515a8ecdd9e2036d1e76f4ae21e93dc8d
Zeshan Shaikh <zee.shaikh0401@gmail.com>
2020-05-13 06:27:00 +0530

    Updates

---
+++ precache-manifest.4b2de92e4b17c833ed81708c7b3e823a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44a86900346a82c5a4862418b999aaea",
+    "url": "/Youtube-Videos/index.html"
+  },
+  {
+    "revision": "d0e72cf2ce793a02a87e",
+    "url": "/Youtube-Videos/static/css/main.3a8ede6a.chunk.css"
+  },
+  {
+    "revision": "60671e5f5620ef6b85d7",
+    "url": "/Youtube-Videos/static/js/2.b83d4397.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Youtube-Videos/static/js/2.b83d4397.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0e72cf2ce793a02a87e",
+    "url": "/Youtube-Videos/static/js/main.96460a31.chunk.js"
+  },
+  {
+    "revision": "dd107a61ed8439a60973",
+    "url": "/Youtube-Videos/static/js/runtime-main.a7cb615c.js"
+  }
+]);
